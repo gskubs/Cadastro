906b2cac35687269c3d42001a4a98354cacacc5a
gskubs <guilherme.skubs@gmail.com>
2026-01-28 23:37:28 -0300

    Meu primeiro commit

---
+++ src/main/java/dev/skbconsultoria/CadastroDeFamilia/CadastroDeFamiliaApplication.java
@@ -0,0 +1,13 @@
+package dev.skbconsultoria.CadastroDeFamilia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeFamiliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeFamiliaApplication.class, args);
+	}
+
+}
